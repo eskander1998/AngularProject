0b23909b476f7f15b9522f57362dcf21c6fda913
unknown <eskander.jguirim@esprit.tn>
2021-12-13 21:40:45 +0100

    ajout statistique

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -11,8 +11,15 @@ import { CustomerService } from 'src/app/services/customer.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+
+  list:Customer[]= [];
+  listInitial: Customer[]= [];
+
   @ViewChild("chart") chart: ChartComponent;
   public chartOptions;
+
+  @ViewChild("chartProfession") chartProfession: ChartComponent;
+  public chartOptionsProfession;
 
   public doughnutChartLabels: any[] = [];
   public doughnutChartData: any = [];
@@ -21,6 +28,14 @@ export class DashboardComponent implements OnInit {
   nbClientOrdinaire:number=0;
   nbClientFidele:number=0;
   nbClientPremium:number=0;
+
+
+  DOCTEUR :number=0;
+  INGENIEUR:number=0;
+  ETUDIANT:number=0;
+  COMMERCIAL:number=0;
+  CADRE:number=0;
+  AUTRE:number=0;
 
   constructor(private cs:CustomerService,private modalService: NgbModal ,private ac:ActivatedRoute,private router: Router) 
 {
@@ -41,10 +56,12 @@ export class DashboardComponent implements OnInit {
           //charts
           
      this.chartOptions = {
-      series: [this.nbClientFidele,this.nbClientOrdinaire,this.nbClientPremium],
+      series: [this.nbClientOrdinaire,this.nbClientFidele,this.nbClientPremium],
       chart: {
-        width: 380,
-        type: "pie"
+        width: 350,
+
+        type: "pie",
+
       },
       labels: ["Fidele", "Ordinaire", "Premium"],
       responsive: [
@@ -61,6 +78,123 @@ export class DashboardComponent implements OnInit {
         }
       ]
     };  }) }) })
+
+
+
+
+    //stat Profession
+
+    this.cs.getAllCustomersFromDB().subscribe(res=> {this.list = res,
+      this.listInitial=this.list;
+     console.log(this.list);
+
+     for(let k in this.list){
+       if(this.list[k].profession=="DOCTEUR")
+       { this.DOCTEUR=this.DOCTEUR+1;
+       console.log("DOCTEUR"+this.DOCTEUR);
+       }
+        if(this.list[k].profession=="INGENIEUR")
+      { this.INGENIEUR=this.INGENIEUR+1;
+       console.log("INGENIEUR"+this.INGENIEUR);
+      }
+        if(this.list[k].profession=="ETUDIANT")
+       {this.ETUDIANT=this.ETUDIANT+1;
+       console.log("ETUDIANT"+this.ETUDIANT);
+      }
+        if(this.list[k].profession=="COMMERCIAL")
+       {this.COMMERCIAL=this.COMMERCIAL+1;
+       console.log("COMMERCIAL"+this.COMMERCIAL);
+      }
+        if(this.list[k].profession=="CADRE")
+       {this.CADRE=this.CADRE+1;
+       console.log("CADRE"+this.CADRE);
+      }
+        if(this.list[k].profession=="AUTRE")
+       {this.AUTRE=this.AUTRE+1;
+       console.log("AUTRE"+this.AUTRE);
+      }
+
+     }
+
+
+     
+this.chartOptionsProfession = {
+  series: [{
+    name: '',
+    data:[this.DOCTEUR,this.INGENIEUR,this.ETUDIANT,this.COMMERCIAL,this.CADRE,this.AUTRE]
+  }],
+  chart: {
+    height: 250,
+    type: 'bar',
+    barWidth:100,
+
+  },
+  plotOptions: {
+    bar: {
+      borderRadius: 10,
+      barWidth:100,
+      dataLabels: {
+        position: 'center', // top, center, bottom
+      },
+    }
+  },
+  dataLabels: {
+    enabled: true,
+    formatter: function (val) {
+      return val;
+    },
+    offsetY: -20,
+    style: {
+      fontSize: '12px',
+      colors: ["#304758"]
+    }
+  },
+  
+  xaxis: {
+    categories: ["DOCTEUR", "INGENIEUR", "ETUDIANT", "COMMERCIAL", "CADRE", "AUTRE"],
+    position: 'top',
+    axisBorder: {
+      show: true
+    },
+    axisTicks: {
+      show: false
+    },
+    crosshairs: {
+      fill: {
+        type: 'gradient',
+        gradient: {
+          colorFrom: '#c6ffdd',
+          colorTo: '#f7797d',
+          stops: [0, 100],
+          opacityFrom: 0.4,
+          opacityTo: 0.5,
+        }
+      }
+    },
+    tooltip: {
+      enabled: true,
+    }
+  },
+  yaxis: {
+    axisBorder: {
+      show: false
+    },
+    axisTicks: {
+      show: false,
+    },
+    labels: {
+      show: false,
+      formatter: function (val) {
+        return val ;
+      }
+    }
+  
+  },
+
+  };
+
+    })
   }
 
 }
+
